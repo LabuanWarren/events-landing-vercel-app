1baafd3acf858651162949f232accdedcb78883b
LabuanWarren <labuanwarren@gmail.com>
2025-10-26 15:48:10 +0800

    ui fixes

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,8 +1,8 @@
 import { Link } from 'react-router-dom';
 export const Footer = () => {
   return (
-    <footer className="flex flex-col justify-start items-center py-20 px-16 md:py-20 md:px-16 py-12 px-8 gap-20 md:gap-20 gap-12 bg-[#011b24]">
-      <div className="flex flex-col justify-start items-center gap-20 md:gap-20 gap-12 max-w-[1280px] w-full">
+    <footer className="flex flex-col justify-start items-center gap-12 bg-brand-950 pt-[40px] md:pt[80px] lg:pt-[120px]">
+      <div className="flex flex-col justify-start items-center gap-20 md:gap-20 gap-12 w-full">
         <div className="flex flex-col justify-start items-center gap-8">
           <div className="flex flex-row justify-start items-start">
             <div className="h-auto w-full flex items-center justify-center rounded">
@@ -21,10 +21,10 @@ export const Footer = () => {
               About Us
             </Link>
             <a className="text-left align-top text-sm font-['DM_Sans'] leading-5 text-white no-underline cursor-pointer transition-colors duration-200 hover:text-[#0a7a94]">
-              Contact
+              Contact Us
             </a>
             <a className="text-left align-top text-sm font-['DM_Sans'] leading-5 text-white no-underline cursor-pointer transition-colors duration-200 hover:text-[#0a7a94]">
-              Blog
+              Blogs
             </a>
             <a className="text-left align-top text-sm font-['DM_Sans'] leading-5 text-white no-underline cursor-pointer transition-colors duration-200 hover:text-[#0a7a94]">
               Careers
@@ -35,8 +35,8 @@ export const Footer = () => {
           </nav>
         </div>
         <div className="flex flex-col justify-start items-center gap-8 w-full">
-          <div className="h-0 w-full max-w-[1280px] border-t border-[#0a7a94]" />
-          <div className="flex flex-row md:flex-row flex-col justify-between items-start md:items-start items-center w-full gap-4 md:gap-0">
+          <div className="h-0 w-full border-t border-brand-900" />
+          <div className="flex flex-row md:flex-row flex-col justify-between items-start md:items-start items-center w-full gap-4 md:gap-0 px-4 md:px-8 lg:px-24 pb-8">
             <span className="text-left align-top text-sm font-['DM_Sans'] leading-5 text-white">
               © 2025 Typid Events by Night Latte.
             </span>
